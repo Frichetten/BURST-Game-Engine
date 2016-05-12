0e2288d4b331e2cb5f633b51271d0afd6624251f
frich <frich@DESKTOP-UF0LVL9>
2016-05-12 16:47:35 -0500

    Add project files.

---
+++ Hack-n-Slash-Game/Main.cpp
@@ -0,0 +1,28 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	sf::Texture texture;
+	if (!texture.loadFromFile("woodfloor_c.png"))
+		printf("Failure to load file\n");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
